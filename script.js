942ae232391e9ac6a55b55b3fa751b5bd30b599a
Vaishali Gupta <162013691+vaishali312003@users.noreply.github.com>
2024-09-10 09:29:10 +0000

    add some minor changes

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const dateBox = document.getElementById("date-box");
 const listContainer = document.getElementById("list-container");
 const progressBar = document.getElementById("progress-bar");
 const progressText = document.getElementById("progress");
+const darkModeToggle = document.getElementById("dark-mode-toggle");
 
-function addTask(){
-    if(inputBox.value === ''){
+function addTask() {
+    if (inputBox.value === '') {
         alert("You must Write Something");
     } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value + ' (Due: ' + dateBox.value + ')';
         li.setAttribute('data-priority', priorityBox.value);
-
         listContainer.appendChild(li);
 
         let span = document.createElement("span");
@@ -27,26 +27,26 @@ function addTask(){
     updateProgress();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         updateProgress();
-    } else if(e.target.tagName === "SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         updateProgress();
     }
 }, false);
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
+function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
     updateProgress();
 }
 
-function updateProgress(){
+function updateProgress() {
     let totalTasks = listContainer.getElementsByTagName('li').length;
     let completedTasks = listContainer.getElementsByClassName('checked').length;
     let progress = totalTasks ? (completedTasks / totalTasks) * 100 : 0;
@@ -54,4 +54,21 @@ function updateProgress(){
     progressText.innerText = Math.round(progress) + "%";
 }
 
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.setItem('darkMode', 'disabled');
+    }
+}
+
+function loadDarkMode() {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark-mode');
+        darkModeToggle.checked = true;
+    }
+}
+
+loadDarkMode();
 showTask();
